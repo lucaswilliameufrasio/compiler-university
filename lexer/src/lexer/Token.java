5db699fddaa4c9d497a350d788a682fb33f64a30
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-15 01:26:56 -0300

    feat: add class Token to create a token and print it

---
+++ lexer/src/lexer/Token.java
@@ -0,0 +1,27 @@
+package lexer;
+
+public class Token {
+	private Tag tag;
+	private String lexeme;
+
+	public Token(Tag t, String l) {
+		tag = t;
+		lexeme = l;
+	}
+
+	public Tag tag() {
+		return tag;
+	}
+
+	public String lexeme() {
+		return lexeme;
+	}
+
+	@Override
+	public String toString() {
+		// The token ASSIGN will be printed
+		// with that format: <ASSIGN, 'ASSIGN'>
+		return "<" + tag + ", '" + lexeme + "'>";
+	}
+
+}
