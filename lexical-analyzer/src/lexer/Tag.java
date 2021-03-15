b5b4279a5a93eb122ba47678aa6b46746ed69d61
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-15 05:02:53 -0300

    feat: add new reserved words tags

--- lexical-analyzer/src/lexer/Tag.java
+++ lexical-analyzer/src/lexer/Tag.java
@@ -2,8 +2,8 @@ package lexer;
 
 public enum Tag {
 	// Reserved Words
-	PROGRAM("PROGRAM"),
-	BEGIN("BEGIN"), END("END"),
+	PROGRAM("PROGRAM"), BEGIN("BEGIN"), END("END"), INT("INT"), REAL("REAL"), BOOL("BOOL"), TRUE("TRUE"),
+	FALSE("FALSE"), READ("READ"), WRITE("WRITE"),
 	// Assign
 	ASSIGN("ASSIGN"),
 	// Arithmetical Operators
@@ -12,24 +12,23 @@ public enum Tag {
 	OR("OR"), AND("AND"), NOT("NOT"),
 	// Relational Operators
 	LT("LT"), LE("LE"), GT("GT"), GE("GE"), NE("NE"),
-	//Symbols
-	LPAREN("LPAREN"), RPAREN("RPAREN"),
-	COMMA("COMMA"), SEMI("SEMI"),
-	//Literals
+	// Symbols
+	LPAREN("LPAREN"), RPAREN("RPAREN"), COMMA("COMMA"), SEMI("SEMI"),
+	// Literals
 	LINT_INT("LINT_INT"), LINT_REAL("LINT_REAL"),
-	//Identifiers
+	// Identifiers
 	ID("ID"),
 	// Others
 	// EOF = End of File
 	// UNK = Unknown Pattern
 	EOF("EOF"), UNK("UNK");
-	
+
 	private String name;
-	
+
 	private Tag(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name;
