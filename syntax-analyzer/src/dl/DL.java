9058c154046794f3535db1d9d9ee04e9e979ba79
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-18 07:08:40 -0300

    refactor: modify DL file to run Parser

--- syntax-analyzer/src/dl/DL.java
+++ syntax-analyzer/src/dl/DL.java
@@ -5,31 +5,19 @@ import java.io.File;
 import lexer.Lexer;
 import lexer.Tag;
 import lexer.Token;
+import parser.Parser;
 
 public class DL {
 	public static void main(String[] args) {
-		Lexer l = 
-			new Lexer(new File("prog.dl"));
-		Token t = l.nextToken();
-		while ( t.tag() != Tag.EOF ) {
-			System.out.println(t);
-			t = l.nextToken();
-		}
+		Lexer l = new Lexer(new File("prog.dl"));
+		Parser p = new Parser(l);
+		p.parse();
+		System.out.println("finalizado");
 	}
 }
 
-
-
-
-
-
-
-
-/*public class DL {
-public static void main(String[] args) {
-	Token t1 = new Token(Tag.ASSIGN, "=");
-	Token t2 = new Token(Tag.LE, "<=");
-	System.out.println(t1);
-	System.out.println(t2);
-}
-}*/
+/*
+ * public class DL { public static void main(String[] args) { Token t1 = new
+ * Token(Tag.ASSIGN, "="); Token t2 = new Token(Tag.LE, "<=");
+ * System.out.println(t1); System.out.println(t2); } }
+ */
