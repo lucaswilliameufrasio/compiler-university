bbbf006209c4095f9daf091471ac0c2882b09c59
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-18 07:29:57 -0300

    feat: add syntax verification of assign on Parser

--- syntax-analyzer/src/parser/Parser.java
+++ syntax-analyzer/src/parser/Parser.java
@@ -42,27 +42,99 @@ public class Parser {
 		match(Tag.DOT);
 		match(Tag.EOF);
 	}
-	
+
 	private void block() {
 		match(Tag.BEGIN);
-		while(look.tag() != Tag.END) {
+		while (look.tag() != Tag.END) {
 			stmt();
 			match(Tag.SEMI);
 		}
 		match(Tag.END);
 	}
-	
+
 	private void stmt() {
-		switch(look.tag()) {
-		case BEGIN: block(); break;
-		case INT: case REAL: case BOOL:
-			decl(); break;
-		default: error("Comando inválido");
+		switch (look.tag()) {
+		case BEGIN:
+			block();
+			break;
+		case INT:
+		case REAL:
+		case BOOL:
+			decl();
+			break;
+		case ID:
+			assign();
+			break;
+		default:
+			error("Comando inválido");
 		}
 	}
-	
+
 	private void decl() {
 		move();
 		match(Tag.ID);
+	}
+
+	private void assign() {
+		match(Tag.ID);
+		match(Tag.ASSIGN);
+		expr();
+	}
+
+	private void expr() {
+		rel();
+		while (look.tag() == Tag.OR) {
+			move();
+			rel();
+		}
+	}
+
+	private void rel() {
+		arith();
+		while (look.tag() == Tag.LT || look.tag() == Tag.LE || look.tag() == Tag.GT) {
+			move();
+			arith();
+		}
+	}
+
+	private void arith() {
+		term();
+		while (look.tag() == Tag.SUM || look.tag() == Tag.SUB) {
+			move();
+			term();
+		}
+	}
+
+	private void term() {
+		factor();
+		while (look.tag() == Tag.MUL) {
+			move();
+			factor();
+		}
+	}
+
+	private void factor() {
+		switch (look.tag()) {
+		case LPAREN:
+			move();
+			expr();
+			match(Tag.RPAREN);
+			break;
+		case LIT_INT:
+			move();
+			break;
+		case LIT_REAL:
+			move();
+			break;
+		case TRUE:
+		case FALSE:
+			move();
+			break;
+		case ID:
+			match(Tag.ID);
+			break;
+		default:
+			error("expressão inválida");
+		}
 	}
 }
