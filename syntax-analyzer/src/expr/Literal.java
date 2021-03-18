989859defa67889c8b9f451ea659107b78ba8741
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-18 07:57:52 -0300

    feat: add Literal node

---
+++ syntax-analyzer/src/expr/Literal.java
@@ -0,0 +1,24 @@
+package expr;
+
+import javax.swing.text.html.HTML.Tag;
+
+import lexer.Token;
+
+public class Literal extends Expr {
+	public Literal(Token op, Tag type) {
+		super(op, type);
+	}
+
+	@Override
+	public String toString() {
+		switch (op.tag()) {
+		case TRUE:
+			return "true";
+		case FALSE:
+			return "false";
+		default:
+			return op.lexeme();
+		}
+	}
+
+}
