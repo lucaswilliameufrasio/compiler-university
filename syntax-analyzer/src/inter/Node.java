9d797519b84a7e118cb1848971c26957bdb99810
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-14 17:06:38 -0300

    style: lint

--- syntax-analyzer/src/inter/Node.java
+++ syntax-analyzer/src/inter/Node.java
@@ -4,23 +4,23 @@ import java.util.LinkedList;
 
 public class Node {
 	private LinkedList<Node> children = new LinkedList<Node>();
-	
+
 	protected void addChild(Node n) {
 		children.add(n);
 	}
-	
+
 	protected LinkedList<Node> children() {
 		return children;
 	}
-	
+
 	public String strTree() {
 		return strStree("");
 	}
-	
+
 	private String strStree(String ident) {
 		StringBuffer stringBuffer = new StringBuffer();
 		stringBuffer.append(toString());
-		for(Node node: children()) {
+		for (Node node : children()) {
 			stringBuffer.append("\n" + ident + "|--> ");
 			stringBuffer.append(node.strStree(ident + "    "));
 		}
