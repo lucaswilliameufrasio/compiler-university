df9e808f9af01c9cb48d13f8f7740b0a49c8abab
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-15 06:46:31 -0300

    feat: generate intermediate code from BLOCK commands

--- intermediate-code/from-dl/src/inter/stmt/Block.java
+++ intermediate-code/from-dl/src/inter/stmt/Block.java
@@ -1,12 +1,23 @@
 package inter.stmt;
 
+import java.util.LinkedList;
+
 public class Block extends Stmt {
-	
+	private LinkedList<Stmt> stmts;
+
 	public Block() {
+		stmts = new LinkedList<Stmt>();
 	}
-	
+
 	public void addStmt(Stmt stmt) {
+		stmts.add(stmt);
 		addChild(stmt);
+	}
+
+	@Override
+	public void gen() {
+		for (Stmt s : stmts)
+			s.gen();
 	}
 
 	@Override
